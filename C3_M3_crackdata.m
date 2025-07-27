1aa8734eacd462e4819df652cdeacce0d0b5570b
Bathiya Dissanayake <74302176+bathiy@users.noreply.github.com>
2025-07-27 21:12:11 +0530

    Delete C3_M3_crackdata.m

--- C3_M3_crackdata.m
+++
@@ -1,8 +0,0 @@
-load CrackData.mat;
-histogram(crackData.Area, 30);
-cutoffregion = 4000;
-crackData.Risk = discretize(crackData.Area, [0, cutoffregion, inf], "categorical", ["Mild", "Severe"]);
-crackinfo = imageDatastore(crackData.fileName(crackData.Risk == "Severe"));
-
-selectedrows = crackData.Risk == "Severe";
-severcrack = crackData(selectedrows, :);
