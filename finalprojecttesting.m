30db1f547f8c1394ca08958161aed3d21d9ba758
Bathiya Dissanayake <74302176+bathiy@users.noreply.github.com>
2025-07-27 21:13:30 +0530

    Delete finalprojecttesting.m

--- finalprojecttesting.m
+++
@@ -1,8 +0,0 @@
-video = VideoReader("RadioTraffic_NNGray.mp4");
-background = im2double(im2gray(read(video, 1)));
-frame = im2double(im2gray(read(video, 151)));
-
-diff = abs(frame - background);
-
-img = finalProjectMask(diff);
-imshow(img)
